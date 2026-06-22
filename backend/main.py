8badc2d075ad24977e572ef5455d43b121b2f942
Spandan sil <spandansil04@gmail.com>
2026-06-22 16:34:40 +0530

    day 4(gemini integration)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware   
+
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
 
 app = FastAPI()
 
@@ -16,12 +29,27 @@ class ChatRequest(BaseModel):
     message: str
 
 
-@app.get("/")
-def home():
-    return {"message": "SignalSense is running!"}
-
 @app.post("/chat")
 def chat(request: ChatRequest):
+    prompt = f"""
+You are SignalSense, an AI learning assistant designed specifically for Electronics and Telecommunication Engineering students.
+
+Your goal is to help students understand concepts intuitively.
+
+Rules:
+- Explain clearly and simply.
+- Use examples whenever possible.
+- Avoid markdown formatting.
+- Avoid symbols like ## and **.
+- Keep responses structured and readable.
+- If the user asks for notes, provide detailed academic style notes.
+- If the user asks for explanation, explain in depth.
+
+User Query:
+{request.message}
+"""
+    response = model.generate_content(prompt)
+
     return {
-        "response": f"You said: {request.message}"
+        "response": response.text
     }
